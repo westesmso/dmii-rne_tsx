b0bddf3281ba0923ca3e676972a5f1aba6dab50a
westesmso <mesquita.westesoficial@gmail.com>
2026-04-15 17:11:28 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {Slider} from "react-native-elements";
+// Define the props for the SliderComponent
+type SliderComponentProps = {};
 
-export default function App() {
+// Main App component
+const AppColorRGB: React.FunctionComponent<SliderComponentProps> = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Escala de cores RGB</Text>
+      <View
+        style={[
+          styles.colorBox,
+          { backgroundColor: `rgb(${red}, ${green}, ${blue})` },
+        ]}
+      />
+      <Text style={styles.label}>Vermelho: {red}</Text>
+      <Slider
+        value={red}
+        onValueChange={(value) => setRed(value)}
+        minimumValue={0}
+        maximumValue={255}
+        step={1}
+        thumbStyle={styles.thumb}
+        trackStyle={styles.track}
+      />
+      <Text style={styles.label}>Verde: {green}</Text>
+      <Slider
+        value={green}
+        onValueChange={(value) => setGreen(value)}
+        minimumValue={0}
+        maximumValue={255}
+        step={1}
+        thumbStyle={styles.thumb}
+        trackStyle={styles.track}
+      /> 
+      <Text style={styles.label}>Azul: {blue}</Text>
+      <Slider
+        value={blue}
+        onValueChange={(value) => setBlue(value)}
+        minimumValue={0}
+        maximumValue={255}
+        step={1}
+        thumbStyle={styles.thumb}
+        trackStyle={styles.track}
+      />  
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  header:{
+    fontSize:24,
+    marginBottom:20,
+  },
+  colorBox: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  thumb: {
+    height: 20,
+    width: 20,
+    backgroundColor: "blue",
+  },
+  track: {
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "lightgray",
   },
 });
+export default AppColorRGB;
